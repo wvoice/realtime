c6f8dadecd0c10dd6e77266d8304896d701ca1ce
Mike Young <myoung@wildernessvoice.com>
2013-09-19 13:51:20 -0700

    Admin Installer Code

---
+++ AOC.2/Installer/Realtime Admin/PROJECT_ASSISTANT/INTERNET_IMAGE/DiskImages/DISK1/program files/Docugate/setup32.cmd
@@ -0,0 +1,52 @@
+:: Register the sql related DLLs
+regsvr32 /s Dependencies\sqldmo\SQLDMO.DLL
+regsvr32 /s Dependencies\sqldmo\sqlresld.dll
+regsvr32 /s Dependencies\sqldmo\sqlsvc.dll
+regsvr32 /s Dependencies\sqldmo\w95scm.DLL
+regsvr32 /s Dependencies\sqldmo\Sqlsvc.rll
+regsvr32 /s Dependencies\sqldmo\Sqldmo.rll
+regsvr32 /s Dependencies\sqldmo\Sqlunirl.dll
+
+:: Register the DLL files
+
+regsvr32 /s Dependencies\actrpt.dll
+
+regsvr32 /s Dependencies\pdfexpt.dll
+
+regsvr32 /s Dependencies\SkinCrafter2.dll
+
+regsvr32 /s Dependencies\SkinCrafterDLL.dll
+
+regsvr32 /s Dependencies\SSubTmr6.dll
+
+
+
+:: Register dependent OCX files
+
+regsvr32 /s Dependencies\CoolXPButton.ocx
+regsvr32 /s Dependencies\CoolXPPanel.ocx
+
+regsvr32 /s Dependencies\SCLabel.ocx
+
+regsvr32 /s Dependencies\ctCheck.ocx
+
+regsvr32 /s Dependencies\ctRadio.ocx
+
+regsvr32 /s Dependencies\vbalExpBar6.ocx
+
+regsvr32 /s Dependencies\vbalGrid6.ocx
+
+regsvr32 /s Dependencies\vbalIml6.ocx
+
+
+
+:: Copy & Register COM files
+
+copy Dependencies\system32\MSCOMCTL.OCX \WINDOWS\system32\
+
+copy Dependencies\system32\MSCOMCT2.OCX \WINDOWS\system32\
+
+regsvr32 /s \WINDOWS\system32\MSCOMCTL.OCX
+
+regsvr32 /s \WINDOWS\system32\MSCOMCT2.OCX
+
